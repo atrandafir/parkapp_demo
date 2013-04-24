1381788ccebb57b03402d5a0db9e7e6d7efa2b68
Alexandru Trandafir Catalin <atrandafirc@gmail.com>
2013-04-24 23:47:31 +0200

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ParkappDemo::Application.config.secret_token = 'e333e29a71de7f49d4f4dd98effabf704d075c57fc786a0f98770482d3627042c2ca8ddc98852261737417703bba7e89c0d1dc3ee287a0dfe2383f5fa7e99237'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+ParkappDemo::Application.config.session_store :cookie_store, key: '_parkapp_demo_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# ParkappDemo::Application.config.session_store :active_record_store
